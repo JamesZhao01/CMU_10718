491b9ee1d50d85d8bd81b9fe50a87bed744702dc
James Zhao <jamezrollz@gmail.com>
2024-10-06 19:41:01 -0400

    evaluator changes

--- src/utils/evaluator.py
+++ src/utils/evaluator.py
@@ -9,6 +9,7 @@ import math
 
 MAX_ANIME_COUNT = 12_294
 
+
 class Type(Enum):
     TV = 1
     OVA = 2
@@ -17,6 +18,7 @@ class Type(Enum):
     ONA = 5
     Music = 6
     Unknown = 7
+
 
 class Genre(Enum):
     Drama = 1
@@ -63,27 +65,46 @@ class Genre(Enum):
     Yaoi = 42
     Yuri = 43
 
+
 class Anime:
-    def __init__(self, id:int, name:str, genres:set[Genre], type:Type, episodes:int, rating:float, membership_count:int):
-        self.id:int = id
-        self.name:str = name
-        self.genres:set[Genre] = genres
-        self.type:Type = type
-        self.episodes:int = episodes
-        self.rating:float = rating
-        self.membership_count:int = membership_count
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        genres: set[Genre],
+        type: Type,
+        episodes: int,
+        rating: float,
+        membership_count: int,
+    ):
+        self.id: int = id
+        self.name: str = name
+        self.genres: set[Genre] = genres
+        self.type: Type = type
+        self.episodes: int = episodes
+        self.rating: float = rating
+        self.membership_count: int = membership_count
+
 
 class User:
-    def __init__(self, id:int, watch_history:np.ndarray[int], rating_history:np.ndarray[float], k):
-        self.id:int = id
-        self.watch_history:np.ndarray[int] = watch_history
+    def __init__(
+        self,
+        id: int,
+        watch_history: np.ndarray[int],
+        rating_history: np.ndarray[float],
+        k,
+    ):
+        self.id: int = id
+        self.watch_history: np.ndarray[int] = watch_history
         # Usually, ratings are ints, but if we normalize the unrated shows to the average
         # they will be a float.
         # self.rating_history:list[float] = rating_history
-        self.rating_history:np.ndarray[float] = rating_history
+        self.rating_history: np.ndarray[float] = rating_history
+        self.imputd_rating_history = np.array([])
         self.k = k
-        self.masked_watch_history:np.ndarray[int] = np.array([])
-        self.masked_rating_history:np.ndarray[float] = np.array([])
+        self.masked_watch_history: np.ndarray[int] = np.array([])
+        self.masked_rating_history: np.ndarray[float] = np.array([])
+        self.masked_imputed_history: np.ndarray[float] = np.array([])
 
     def generate_masked_history(self):
         """
@@ -91,19 +112,22 @@ class User:
         Note that this should be called after the filtering process, so that
         the masked information does not include any filtered out data.
         """
-        
-        # Randomly select indices to mask (subsample)
-        mask = np.random.choice(len(self.watch_history), size=self.k, replace=False)
-        preserved_data = np.setdiff1d(np.arange(len(self.watch_history)), mask)
-        
-        # Subsample both arrays using the selected indices
-        self.masked_watch_history = self.watch_history[preserved_data]
-        self.masked_rating_history = self.rating_history[preserved_data]
+        if len(self.masked_watch_history) == 0:
+            # Randomly select indices to mask (subsample)
+            self.mask = np.random.choice(
+                len(self.watch_history), size=self.k, replace=False
+            )
+            preserved_data = np.setdiff1d(np.arange(len(self.watch_history)), self.mask)
+
+            # Subsample both arrays using the selected indices
+            self.masked_watch_history = self.watch_history[preserved_data]
+            self.masked_rating_history = self.rating_history[preserved_data]
+            self.masked_imputed_history = self.imputed_rating_history[preserved_data]
 
     def get_history(self):
         """
         Returns a one-hot encoding of the rating history of the user.
-        The index is the anime id. 
+        The index is the anime id.
         (Note that index 0 is always just 0 since anime id starts with 1).
         """
 
@@ -111,7 +135,13 @@ class User:
         # Populate the array with ratings at indices corresponding to anime_id
         one_hot_encoding[self.watch_history] = self.rating_history
         return one_hot_encoding
-    
+
+    def get_imputed_history(self):
+        one_hot_encoding = np.zeros(MAX_ANIME_COUNT, dtype=float)
+        # Populate the array with ratings at indices corresponding to anime_id
+        one_hot_encoding[self.watch_history] = self.imputed_rating_history
+        return one_hot_encoding
+
     def get_masked_history(self):
         """
         Same as get_history, but some of the information is masked out.
@@ -123,118 +153,148 @@ class User:
 
         one_hot_encoding[self.masked_watch_history] = self.masked_rating_history
         return one_hot_encoding
-        
+
+    def get_imputed_masked_history(self):
+        one_hot_encoding = np.zeros(MAX_ANIME_COUNT, dtype=float)
+        one_hot_encoding[self.masked_watch_history] = self.masked_imputed_history
+        return one_hot_encoding
+
 
 class Evaluator:
-    def __init__(self, path: str, normalize_unrated = True):
-        self.animes = pd.read_csv(os.path.join(path, 'anime.csv'))
-        self.users = pd.read_csv(os.path.join(path, 'rating.csv'))
+    def __init__(self, path: str, normalize_unrated=True):
+        self.animes = pd.read_csv(os.path.join(path, "anime.csv"))
+        self.users = pd.read_csv(os.path.join(path, "rating.csv"))
         self.k = 10
         self.threshold_watch_history = 20
         self.user_mapping = {}
         self.anime_mapping = {}
+        self.canonical_id_to_anime_id = {}
+        self.anime_id_to_canonical_id = {}
         self.user_ids = []
         self.current_idx = 0
         self.normalize_unrated = normalize_unrated
+
+        print(f"{normalize_unrated=}")
         np.random.seed(42)
 
         # Gets all the information about the animes
         for anime_id, anime_df in self.animes.iterrows():
-
             genres = []
-            if not pd.isna(anime_df['genre']):
-                for unprocessed_string in anime_df['genre'].split(','):
+            if not pd.isna(anime_df["genre"]):
+                for unprocessed_string in anime_df["genre"].split(","):
                     stripped = unprocessed_string.strip()
-                    if stripped == 'Sci-Fi':
-                        genre = 'Sci_Fi'
-                    elif stripped == 'Super Power':
-                        genre = 'Super_Power'
-                    elif stripped == 'Slice of Life':
-                        genre = 'Slice_of_Life'
-                    elif stripped == 'Shounen Ai':
-                        genre = 'Shounen_Ai'
-                    elif stripped == 'Shoujo Ai':
-                        genre = 'Shoujo_Ai'
-                    elif stripped == 'Martial Arts':
-                        genre = 'Martial_Arts'
+                    if stripped == "Sci-Fi":
+                        genre = "Sci_Fi"
+                    elif stripped == "Super Power":
+                        genre = "Super_Power"
+                    elif stripped == "Slice of Life":
+                        genre = "Slice_of_Life"
+                    elif stripped == "Shounen Ai":
+                        genre = "Shounen_Ai"
+                    elif stripped == "Shoujo Ai":
+                        genre = "Shoujo_Ai"
+                    elif stripped == "Martial Arts":
+                        genre = "Martial_Arts"
                     else:
                         genre = stripped
-                    genres.append(getattr(Genre,genre))
+                    genres.append(getattr(Genre, genre))
 
-            if pd.isna(anime_df['type']):
-                type = 'Unknown'
+            if pd.isna(anime_df["type"]):
+                type = "Unknown"
             else:
-                type = anime_df['type']
+                type = anime_df["type"]
             type = getattr(Type, type)
 
-            if pd.isna(anime_df['rating']):
-                rating = 6.57 #median value
+            if pd.isna(anime_df["rating"]):
+                rating = 6.57  # median value
             else:
-                rating = anime_df['rating']
+                rating = anime_df["rating"]
 
-            if pd.isna(anime_df['members']):
+            if pd.isna(anime_df["members"]):
                 membership_count = -1
             else:
-                membership_count = anime_df['members']
+                membership_count = anime_df["members"]
 
-            anime = Anime(id = anime_df['anime_id']-1,
-                  name = anime_df['name'],
-                  genres = genres,
-                  type = type,
-                  episodes = anime_df['episodes'],
-                  rating = rating,
-                  membership_count = membership_count
-                  )
-            self.anime_mapping[anime_df['anime_id']-1] = anime
+            anime = Anime(
+                id=anime_df["anime_id"] - 1,
+                name=anime_df["name"],
+                genres=genres,
+                type=type,
+                episodes=anime_df["episodes"],
+                rating=rating,
+                membership_count=membership_count,
+            )
+            self.anime_mapping[anime_df["anime_id"] - 1] = anime
+        self.anime_id_to_canonical_id = {
+            k: i for i, k in enumerate(sorted(self.anime_mapping.keys()))
+        }
+        self.canonical_id_to_anime_id = {
+            v: k for k, v in self.anime_id_to_canonical_id.items()
+        }
 
         # Gets all the user watch history
-        for user_id, user_df in self.users.groupby('user_id'):
-            anime_list:np.ndarray[int] = np.array(user_df['anime_id'].tolist(), dtype=int)
+        for user_id, user_df in self.users.groupby("user_id"):
+            anime_list: np.ndarray[int] = np.array(
+                user_df["anime_id"].tolist(), dtype=int
+            )
             anime_list = anime_list - 1
-            rating_list:np.ndarray[float] = np.array(user_df['rating'].tolist(), dtype=float)
+            rating_list: np.ndarray[float] = np.array(
+                user_df["rating"].tolist(), dtype=float
+            )
 
             assert len(anime_list) == len(rating_list)
 
             # Filters out ratings that aren't valid with our anime_list
             valid_indices = np.where(anime_list < MAX_ANIME_COUNT)[0]
-            anime_list:np.ndarray[int] = anime_list[valid_indices]
-            rating_list:np.ndarray[float] = rating_list[valid_indices]
+            anime_list: np.ndarray[int] = anime_list[valid_indices]
+            rating_list: np.ndarray[float] = rating_list[valid_indices]
 
             user = User(user_id, anime_list, rating_list, self.k)
 
             # Filter/Preprocess the data
-            filtered_watch_history:list[int] = []
-            filtered_rating_history:list[int] = []
-            for i,anime_id in enumerate(user.watch_history):
+            filtered_watch_history: list[int] = []
+            filtered_rating_history: list[int] = []
+            imputed_rating_history: list[int] = []
+            for i, anime_id in enumerate(user.watch_history):
                 # Filter out NSFW
                 current_anime = self.anime_mapping[anime_id]
                 if Genre.Hentai not in current_anime.genres:
                     filtered_watch_history.append(anime_id)
-                    filtered_rating_history.append(user.rating_history[i])
-
-                    if normalize_unrated and user.rating_history[i] == -1:
-                        filtered_rating_history[-1] = self.anime_mapping[anime_id].rating
+                    filtered_rating_history.append(
+                        self.anime_mapping[anime_id].rating
+                        if normalize_unrated and user.rating_history[i] == -1
+                        else user.rating_history[i]
+                    )
+                    imputed_rating_history.append(
+                        self.anime_mapping[anime_id].rating
+                        if user.rating_history[i] == -1
+                        else user.rating_history[i]
+                    )
 
             user.watch_history = np.array(filtered_watch_history)
             user.rating_history = np.array(filtered_rating_history)
+            user.imputed_rating_history = np.array(imputed_rating_history)
 
             # Filters out users that have too little watch history.
-            enough_watch_history = np.count_nonzero(user.watch_history) >= self.threshold_watch_history
+            enough_watch_history = (
+                np.count_nonzero(user.watch_history) >= self.threshold_watch_history
+            )
             if enough_watch_history:
                 self.user_mapping[user.id] = user
-                
+                # We need the history
+                user.generate_masked_history()
 
         self.idxs = list(self.user_mapping.keys())
-        
+
         # Split the data into train/test/val
         train_size = int(0.8 * len(self.idxs))
         val_size = int(0.1 * len(self.idxs))
 
         self.train_indices = self.idxs[:train_size]
-        self.val_ids = self.idxs[train_size:train_size + val_size]
-        self.test_ids = self.idxs[train_size + val_size:]
+        self.val_ids = self.idxs[train_size : train_size + val_size]
+        self.test_ids = self.idxs[train_size + val_size :]
 
-    def get_anime_info(self, id:int):
+    def get_anime_info(self, id: int):
         return self.anime_mapping[id]
 
     ###################################################################################
@@ -246,9 +306,9 @@ class Evaluator:
         You MUST call end_eval_runtime() as soon as possible to get evaluation results.
         """
         user_id = self.test_ids[self.current_idx]
-        user:User = self.user_mapping[user_id]
+        user: User = self.user_mapping[user_id]
         user.generate_masked_history()
-        user_masked_watch_history = user.get_masked_history()
+        user_masked_watch_history = user.get_imputed_masked_history()
         self.start_time = time.time()
         return user_masked_watch_history[None, ...], self.k
 
@@ -263,11 +323,12 @@ class Evaluator:
 
         k_recommended_shows = k_recommended_shows.squeeze()
         user_id = self.test_ids[self.current_idx]
-        user:User = self.user_mapping[user_id]
+        user: User = self.user_mapping[user_id]
         complete_watch_history = user.get_history()
         user_masked_watch_history = user.get_masked_history()
-        ground_truth_of_masked_history = complete_watch_history - user_masked_watch_history
-        
+        ground_truth_of_masked_history = (
+            complete_watch_history - user_masked_watch_history
+        )
 
         pred = np.zeros(MAX_ANIME_COUNT, dtype=int)
         # Sort the indices and generate decreasing values
@@ -276,59 +337,66 @@ class Evaluator:
         # Assign the decreasing values to the respective indices
         pred[k_recommended_shows] = decreasing_values
 
-        score = ndcg_score(ground_truth_of_masked_history[None,...], pred[None,...], k=self.k)
-        print(f'This model took {total_runtime} seconds.')
-        print(f'Out of an optimal score of 1.0, you scored {score}.')
+        score = ndcg_score(
+            ground_truth_of_masked_history[None, ...], pred[None, ...], k=self.k
+        )
+        print(f"This model took {total_runtime} seconds.")
+        print(f"Out of an optimal score of 1.0, you scored {score}.")
         return total_runtime, score
+
     ####################################################################################
-    
-    
+
     def start_eval_test_set(self):
         """
-        Returns: 
+        Returns:
         - user_masked_watch_history: Masked user watch history of shape [num_of_users, MAX_ANIME_COUNT]
         - k: number of anime recommendations your model should present
         """
-        
+
         user_masked_watch_history = np.zeros((len(self.test_ids), MAX_ANIME_COUNT))
-        for i,id in enumerate(self.test_ids):
-            user = self.user_mapping[id]
+        for i, id in enumerate(self.test_ids):
+            user: User = self.user_mapping[id]
             user.generate_masked_history()
             user_masked_watch_history[i] = user.get_masked_history()
 
         self.start_time = time.time()
         return user_masked_watch_history, self.k
-    
-    
+
     def end_eval_test_set(self, k_recommended_shows: np.ndarray[int]):
         """
         Run this method to end the evaluation program.
         I expect k_recommended_shows to be of shape [test_set_size, k].
         The columns should be ordered by ranking with 0 being the highest and 1 being
         the lowest.
+
+        if is_anime_id is true, then each row of k_recommended_shows should be a list of anime ids,
+            in ranked order
         """
         total_runtime = time.time() - self.start_time
 
         complete_watch_history = np.zeros((len(self.test_ids), MAX_ANIME_COUNT))
         user_masked_watch_history = np.zeros((len(self.test_ids), MAX_ANIME_COUNT))
         ground_truth_of_masked_history = np.zeros((len(self.test_ids), MAX_ANIME_COUNT))
-        for i,id in enumerate(self.test_ids):
-            user = self.user_mapping[id]
-            complete_watch_history[i] = user.get_history()
-            user_masked_watch_history[i] = user.get_masked_history()
-            ground_truth_of_masked_history[i] = complete_watch_history[i] - user_masked_watch_history[i]
-        
+        for i, id in enumerate(self.test_ids):
+            user: User = self.user_mapping[id]
+            complete_watch_history[i] = user.get_imputed_history()
+            user_masked_watch_history[i] = user.get_imputed_masked_history()
+            ground_truth_of_masked_history[i] = (
+                complete_watch_history[i] - user_masked_watch_history[i]
+            )
 
         pred = np.zeros((len(self.test_ids), MAX_ANIME_COUNT), dtype=int)
+
         # Sort the indices and generate decreasing values
         decreasing_values = np.arange(k_recommended_shows.shape[1], 0, -1)
 
-
         # Assign the decreasing values to the respective indices
         # pred[np.array(k_recommended_shows)] = decreasing_values
-        pred[np.arange(len(self.test_ids))[:, None], k_recommended_shows] = decreasing_values
+        pred[np.arange(len(self.test_ids))[:, None], k_recommended_shows] = (
+            decreasing_values
+        )
 
         score = ndcg_score(ground_truth_of_masked_history, pred, k=self.k)
-        print(f'This model took {total_runtime} seconds.')
-        print(f'Out of an optimal score of 1.0, you scored {score}.')
+        print(f"This model took {total_runtime:0.4f} seconds.")
+        print(f"Out of an optimal score of 1.0, you scored {score:0.4f}.")
         return total_runtime, score
